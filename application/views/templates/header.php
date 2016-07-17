c57484b86c27b57393c24f3c06f14db4c543fde1
FURIBAITO <furibaito@gmail.com>
2016-07-17 23:03:35 +0700

    Revert "Revert "Cleaned up some files""
    This reverts commit ea5837bb001127ba2f92e6f5cdc4776e444f27f6.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,8 +8,9 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap-theme.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/qunit.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/header.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/media.css">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
         <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery-2.0.2.js"></script>
         <script type="text/javascript" src="<?php echo base_url() ?>/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url() ?>/js/qunit.js"></script>
@@ -36,22 +37,31 @@
     </head>
 
     <body>
+        <div class="logo-cf-header-container-small mobile-only">
+            <img src="<?php echo base_url() ?>images/biglogo.svg">
+            <div>
+                <p>CHEMISTRY FAIR<br/><span>2016</span></p>
+            </div>
+        </div>
         <div id="top-bar">
-            <div class="col-md-1"></div>
             <div id="logo-univ-header-container" class="col-md-2">
                 <img id="logo-univ-header" src="<?php echo base_url() ?>/images/logoui.png">
             </div>
-            <div class="col-md-8">
+            <div class="col-md-9">
                 <ul class="nav">
                     <li class="col-md-2"><a href="<?php echo base_url() ?>">BERANDA</a></li>
                     <li class="col-md-2"><a href="<?php echo base_url() ?>">ACARA</a></li>
                     <li class="col-md-2"><a href="<?php echo base_url() ?>">LOMBA</a></li>
                     <li class="col-md-3"><a href="<?php echo base_url() ?>">TENTANG KAMI</a></li>
                 </ul>
-                <div id="logo-cf-header-container">
-                    <img src="<?php echo base_url() ?>/images/logo.svg">
-                    <span>CHEMISTRY FAIR 2016</span>
-                </div>
             </div>
-            <div class="col-md-1"></div>
         </div>
+        <div class="logo-cf-header-container">
+            <img src="<?php echo base_url() ?>images/biglogo.svg">
+            <div>
+                <p>CHEMISTRY FAIR<br/><span>2016</span></p>
+            </div>
+        </div>
+        <div id="top-bar">dsdf</div>
+        <div id="top-bar">dsdf</div>
+        <div id="top-bar">dsdf</div>
