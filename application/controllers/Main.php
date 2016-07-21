a6b2fb59122b81f6a43c807c2a43a5c9205002e1
Jonathan Hosea <jonathan.hosea@me.com>
2016-07-21 23:30:33 +0700

    Fixed Index

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -4,47 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 	public function index()
-	{
-			$this->home();
-			exit();
-			
-			$event = mktime(0,0,0,8,1,2016);
-			$remaining = $event - time();
-			$data['countdown'] = floor($remaining / 86400);
-			
-			$page = "landing_page";
-			if (!file_exists (APPPATH.'views/'.$page.'.php'))
-			{
-				//Homepage does not exist
-				show_404();
-			}
-			
-			$data['page_title'] = "Chemistry Fair UI 2016";
-			$this->load->view($page, $data);
-	}
-	
-	public function aboutus()
-	{
-		$event = mktime(0,0,0,8,1,2016);
-		$remaining = $event - time();
-		$data['countdown'] = floor($remaining / 86400);
-		
-		$page = "about_us";	
-		if (!file_exists (APPPATH.'views/'.$page.'.php'))
-			{
-				//Homepage does not exist
-				show_404();
-			}
-			
-			$data['title'] = "Tentang Kami - Chemistry Fair";
-			
-			$this->load->view('templates/header.php', $data);
-			$this->load->view($page, $data);
-			$this->load->view('templates/footer.php');
-	}
-	
-	public function home()
-	{
+	{		
 		$event = mktime(0,0,0,8,1,2016);
 		$remaining = $event - time();
 		$data['countdown'] = floor($remaining / 86400);
@@ -78,6 +38,27 @@ class Main extends CI_Controller {
 			$this->load->view($page, $data);
 			$this->load->view('templates/footer.php');
 	}
+	
+	public function aboutus()
+	{
+		$event = mktime(0,0,0,8,1,2016);
+		$remaining = $event - time();
+		$data['countdown'] = floor($remaining / 86400);
+		
+		$page = "about_us";	
+		if (!file_exists (APPPATH.'views/'.$page.'.php'))
+			{
+				//Homepage does not exist
+				show_404();
+			}
+			
+			$data['title'] = "Tentang Kami - Chemistry Fair";
+			
+			$this->load->view('templates/header.php', $data);
+			$this->load->view($page, $data);
+			$this->load->view('templates/footer.php');
+	}
+
 }
 
 ?>
