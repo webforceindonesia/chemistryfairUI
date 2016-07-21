033b341ca3f9c1b3e5e567c84f9ca20d96f3e6e3
FURIBAITO <furibaito@gmail.com>
2016-07-21 18:58:18 +0700

    Fixed header and landing html

--- application/views/landing_page.php
+++ application/views/landing_page.php
@@ -1,3 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
 <html>
     <head>
 
@@ -8,10 +11,8 @@
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap-theme.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/qunit.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/comingsoon.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/style.css">
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>/css/media.css">
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-        
         <script type="text/javascript" src="<?php echo base_url() ?>/js/jquery-2.0.2.js"></script>
         <script type="text/javascript" src="<?php echo base_url() ?>/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url() ?>/js/qunit.js"></script>
@@ -24,30 +25,18 @@
         ----    Copyright Weborn 2016    --
         ---- Do Not Attempt to Duplicate --
         ---------------------------------->
-        
-        <style>
-            @media (max-width:995px)
-            {
-                .logo_univ
-                {
-                    display:none;	
-                }
-            }
-        </style>
     
     </head>
-
     <body>
-        <div class="logo-cf-header-container-small mobile-only">
-
+        <div class="logo-cf-comingsoon-container-small">
             <img src="<?php echo base_url() ?>images/bigLogo.svg">
             <div>
                 <p>CHEMISTRY FAIR<br/><span>2016</span></p>
             </div>
         </div>
         <div id="top-bar">
-            <div id="logo-univ-header-container" class="col-md-2">
-                <img id="logo-univ-header" src="<?php echo base_url() ?>/images/logoui.png">
+            <div id="logo-univ-comingsoon-container" class="col-md-2">
+                <img id="logo-univ-comingsoon" src="<?php echo base_url() ?>/images/logo_univ_box.png">
                 <img id="logo-hmd" src="<?php echo base_url() ?>images/HMD-Kimia.png">
             </div>
             <div class="col-md-8">
@@ -55,7 +44,7 @@
             </div>
         </div>
 
-        <div class="logo-cf-header-container">
+        <div class="logo-cf-comingsoon-container">
             <img src="<?php echo base_url() ?>images/bigLogo.svg">
             <div>
                 <p>CHEMISTRY FAIR<br/><span>2016</span></p>
@@ -80,3 +69,5 @@
                 </div>
             </div>
         </div>
+    </body>
+</html>
